4183a21f7f9795b1bf0bd13436db3fd0ad3b8f4f
khanh_capy <trandinhkhanh0318@gmail.com>
2025-09-14 22:52:05 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logoImg from "../assets/tuhoc.cc.png";
+export default function Header() {
+  return (
+    <>
+      <header>
+        <img src={logoImg} alt="" />
+        <h1>React Quiz</h1>
+      </header>
+    </>
+  );
+}
